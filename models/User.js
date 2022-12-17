56899ca82d51b9f8e92bf4238a9b2212e4f28ddb
rdahltorp <109421585+rdahltorp@users.noreply.github.com>
2022-12-17 11:35:19 -0700

    Work on the User model.

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose')
+const thoughtSchema = require('./Thought')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            //Need to add in email validation here. 
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+//Virtual to pull number of friends in the user's "friends" array. 
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length
+    });
+
+const User = model('user', userSchema)
